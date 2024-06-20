ec8257253a9800582616ec786a201a96b53052d6
Mathias G <ghbm@aarhus.dk>
2024-06-20 13:21:34 +0200

    Added get journal function

--- robot_framework/sub_process/eflyt.py
+++ robot_framework/sub_process/eflyt.py
@@ -1,6 +1,8 @@
 """This module handles interaction with eFlyt."""
 
 from datetime import date
+import os
+import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -8,6 +10,9 @@ from selenium.webdriver.common.by import By
 from OpenOrchestrator.orchestrator_connection.connection import OrchestratorConnection
 
 from robot_framework import config
+
+
+DOWNLOAD_DIR = os.path.join(os.getcwd(), "downloads")
 
 
 def login(orchestrator_connection: OrchestratorConnection) -> webdriver.Chrome:
@@ -21,7 +26,11 @@ def login(orchestrator_connection: OrchestratorConnection) -> webdriver.Chrome:
     """
     eflyt_creds = orchestrator_connection.get_credential(config.EFLYT_CREDS)
 
-    browser = webdriver.Chrome()
+    options = webdriver.ChromeOptions()
+    options.add_experimental_option("prefs", {"download.default_directory": DOWNLOAD_DIR})
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+    browser = webdriver.Chrome(options)
     browser.maximize_window()
     browser.get("https://notuskommunal.scandihealth.net/")
 
@@ -37,15 +46,15 @@ def login(orchestrator_connection: OrchestratorConnection) -> webdriver.Chrome:
     return browser
 
 
-def search_case_address(browser: webdriver.Chrome, case_number: str) -> str:
-    """Find the address of a given case in eFlyt.
+def search_case_info(browser: webdriver.Chrome, case_number: str) -> tuple[str, str]:
+    """Find the address of a given case in eFlyt and download the case journal.
 
     Args:
         browser: The browser object already logged in to eFlyt.
-        case_number: The case number of the case to find the address for.
+        case_number: The case number of the case to find.
 
     Returns:
-        The address of the given case.
+        The address of the given case and the file path of the downloaded journal.
     """
     browser.maximize_window()
 
@@ -69,6 +78,48 @@ def search_case_address(browser: webdriver.Chrome, case_number: str) -> str:
     address = browser.find_element(By.CSS_SELECTOR, "#ctl00_ContentPlaceHolder1_searchControl_GridViewSearchResult > tbody > tr:nth-child(2) > td:nth-child(5)").text
     address = address.strip()
 
+    journal_path = get_journal(browser)
+
+    # Go back to main page
+    browser.get("https://notuskommunal.scandihealth.net/web/SuperSearch.aspx")
+
     browser.minimize_window()
 
-    return address
+    return address, journal_path
+
+
+def get_journal(browser: webdriver.Chrome) -> str:
+    """Download the case journal.
+
+    Args:
+        browser: The browser object already logged in to eFlyt and with the
+        relevant case in the search results.
+
+    Raises:
+        RuntimeError: If any unexpected files are downloaded.
+        TimeoutError: If the file isn't downloaded within 10 seconds.
+
+    Returns:
+        The path to the downloaded file.
+    """
+    # Clear download dir
+    for f in os.listdir(DOWNLOAD_DIR):
+        os.remove(os.path.join(DOWNLOAD_DIR, f))
+
+    # Open case and get journal
+    browser.execute_script("__doPostBack('ctl00$ContentPlaceHolder1$searchControl$GridViewSearchResult','cmdRowSelected$0')")
+    browser.find_element(By.ID, "ctl00_ContentPlaceHolder2_ptFanePerson_stcPersonTab1_btnJournal").click()
+
+    # Wait for file download
+    for _ in range(10):
+        files = os.listdir(DOWNLOAD_DIR)
+        if files:
+            if len(files) != 1:
+                raise RuntimeError(f"An unexpected number of files where found in the downloads folder: {files}")
+
+            if files[0].endswith(".pdf"):
+                return os.path.join(DOWNLOAD_DIR, files[0])
+
+        time.sleep(1)
+
+    raise TimeoutError("Downloaded file didn't appear within 10 seconds.")
